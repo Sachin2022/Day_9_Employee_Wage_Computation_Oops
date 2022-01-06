ffd5a604037c85e5e3843a031981f5a50df81cf6
Textspot <sachinkatarki1995@gmail.com>
2022-01-06 17:46:56 +0530

    Added:Code to Calculate the Employee Total working Hours

--- src/com/bridgelabz/empwage/EmployeeWageBuilder.java
+++ src/com/bridgelabz/empwage/EmployeeWageBuilder.java
@@ -4,6 +4,8 @@ public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static int MAX_WORKING_DAYS = 20;
+	public static final int MAX_WORKING_HRS = 100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee Wage Computation");
@@ -15,7 +17,10 @@ public class EmployeeWageBuilder {
 		int empHrs = 0;
 		int empwage = 0;
 		int totalWage = 0;
-		for (int i = 1; i <= workingDay; i++) {
+		int totalWorkingHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalWorkingHrs <= MAX_WORKING_HRS && totalWorkingHrs < MAX_WORKING_DAYS) {
+			totalWorkingDays++;
 			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empcheck) {
 			case 1:
@@ -30,10 +35,12 @@ public class EmployeeWageBuilder {
 				System.out.println("Employee is Absent");
 				empHrs = 0;
 			}
+			totalWorkingHrs += empHrs;
 			empwage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Day " + i + " Employee Wage = " + empwage);
+			System.out.println(" Employee Wage = " + empwage);
 			totalWage = totalWage + empwage;
+			System.out.println("Total working hrs = " + totalWorkingHrs);
+			System.out.println("Total Emp wage = " + totalWage);
 		}
-		System.out.println("Total Emp wage = " + totalWage);
 	}
 }
