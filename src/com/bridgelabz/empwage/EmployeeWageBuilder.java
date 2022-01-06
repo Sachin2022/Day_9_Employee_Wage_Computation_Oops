7ad40d8e3ab956eda9c8c51f988b13878fefa5ee
Textspot <sachinkatarki1995@gmail.com>
2022-01-06 17:13:18 +0530

    Added:Code to Calculate the Employee Attendance

--- src/com/bridgelabz/empwage/EmployeeWageBuilder.java
+++ src/com/bridgelabz/empwage/EmployeeWageBuilder.java
@@ -1,10 +1,19 @@
 package com.bridgelabz.empwage;
 
 public class EmployeeWageBuilder {
+	public static final int IS_FULL_TIME = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee Wage Computation");
-
+		empWage();
 	}
 
+	public static void empWage() {
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
+	}
 }
