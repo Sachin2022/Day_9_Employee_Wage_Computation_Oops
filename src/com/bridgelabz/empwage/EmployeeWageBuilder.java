b87e12922b9994e69c0fe10e93f94252c53fcd85
Textspot <sachinkatarki1995@gmail.com>
2022-01-06 17:28:09 +0530

    Added:Code to Calculate the Employee Wage Using Switch Case

--- src/com/bridgelabz/empwage/EmployeeWageBuilder.java
+++ src/com/bridgelabz/empwage/EmployeeWageBuilder.java
@@ -13,18 +13,22 @@ public class EmployeeWageBuilder {
 	public static void empWage() {
 		int empHrs = 0;
 		int empwage = 0;
-		double empcheck = Math.floor(Math.random() * 10) % 3;
-		if (empcheck == IS_FULL_TIME) {
+		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empcheck) {
+		case 1:
 			System.out.println("Employee is Full time ");
 			empHrs = 8;
-		} else if (empcheck == IS_PART_TIME) {
-			System.out.println("Employee is Part time");
+			break;
+		case 2:
+			System.out.println("Employee is Part time ");
 			empHrs = 4;
-		} else {
+			break;
+		default:
 			System.out.println("Employee is Absent");
 			empHrs = 0;
 		}
 		empwage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp wage = " + empwage);
+
 	}
 }
