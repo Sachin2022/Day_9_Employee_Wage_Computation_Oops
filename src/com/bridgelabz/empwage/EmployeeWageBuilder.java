2fa1873762a09aae260db6b270f4bd377001949f
Textspot <sachinkatarki1995@gmail.com>
2022-01-06 17:22:20 +0530

    Added:Code to Calculate the Employee Part Time Wage

--- src/com/bridgelabz/empwage/EmployeeWageBuilder.java
+++ src/com/bridgelabz/empwage/EmployeeWageBuilder.java
@@ -2,6 +2,7 @@ package com.bridgelabz.empwage;
 
 public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 
 	public static void main(String[] args) {
@@ -12,15 +13,18 @@ public class EmployeeWageBuilder {
 	public static void empWage() {
 		int empHrs = 0;
 		int empwage = 0;
-		double empcheck = Math.floor(Math.random() * 10) % 2;
+		double empcheck = Math.floor(Math.random() * 10) % 3;
 		if (empcheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Full time ");
 			empHrs = 8;
+		} else if (empcheck == IS_PART_TIME) {
+			System.out.println("Employee is Part time");
+			empHrs = 4;
 		} else {
 			System.out.println("Employee is Absent");
 			empHrs = 0;
 		}
 		empwage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp wage " + empwage);
+		System.out.println("Emp wage = " + empwage);
 	}
 }
