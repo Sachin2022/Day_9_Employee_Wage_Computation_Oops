1f25e14e989a57fe7dd39631f26765e8bf26547c
Textspot <sachinkatarki1995@gmail.com>
2022-01-06 17:08:51 +0530

    Added:Welcome to the Employee Wages

---
+++ src/com/bridgelabz/empwage/EmployeeWageBuilder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.empwage;
+
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Employee Wage Computation");
+
+	}
+
+}
